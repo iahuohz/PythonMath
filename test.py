2384037aa19736c7e5980de91043488fb1e69df9
iahuohz <iahuohz@hotmail.com>
2018-08-31 11:36:10 +0800

    111

--- test.py
+++ test.py
@@ -1,29 +1,15 @@
 import datetime
 import numpy as np
 
-h, l, c = np.loadtxt('data/data.csv', delimiter=',', usecols=(4, 5, 6), unpack=True)
-N = 5
-h = h[-N:]
-l = l[-N:]
-print("len(h)", len(h), "len(l)", len(l))
-print("Close", c)
-previousclose = c[-N -1: -1]
-print("len(previousclose)", len(previousclose))
-print("Previous close", previousclose)
-print("h - l ", h - l)
-print("h - previousclose", h - previousclose)
-print("previousclose - l", previousclose - l)
-truerange = np.maximum(h - l, h - previousclose, previousclose - l)
-print("True range", truerange)
-atr = np.zeros(N)
-atr[0] = np.mean(truerange)
-for i in range(1, N):
-    atr[i] = (N - 1) * atr[i - 1] + truerange[i]
-    atr[i] /= N
-
-print("ATR", atr)
-from datetime import datetime
-import numpy as np
-import matplotlib.pyplot as plt
+c = np.loadtxt('Numpy_Beginners/data/data.csv', delimiter=',', usecols=(6,), unpack=True)
+# Initialize estimates array
+estimates = np.zeros((len(c), 3))
+for i in np.arange(len(c)):
+    # Create a temporary copy and omit one value
+    a = c.copy()
+    a[i] = np.nan
+    # Compute estimates
+    estimates[i,] = [np.nanmean(a), np.nanvar(a), np.nanstd(a)]
+print("Estimates variance", estimates.var(axis=0))
 
 
